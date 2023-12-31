52c50d2ad49d762bff55ce5661c21f1dadc03e18
John Paul Ernersto Paredes Diez de Medina <125041352+JohnPaul0403@users.noreply.github.com>
2023-12-31 12:25:26 -0400

    Update get_database.py

--- lib/get_database.py
+++ lib/get_database.py
@@ -37,25 +37,6 @@ cursor_as.execute("""
     )
 """)
 
-#Create first user
-#cursor.execute("""INSERT INTO users (name, email, password, pro_plan) VALUES ('admin', 'admin@admin.com', 'admin_pass', 1)""")
-email = 'johnpar2004@gmail.com'
-#cursor.execute("""INSERT INTO users (name, email, password, pro_plan) VALUES ('John Paredes', ?, 'my_pass', 1)""", (email,))
-
-#get_user
-cursor.execute("""SELECT * FROM users""")
-print(cursor.fetchall())
-
-#create assistant
-#cursor_as.execute("INSERT INTO assistants (name, user_id, assistant_id, gpt_model) VALUES ('ProjectBot', 3, 'asst_x824apYmRJibSkPy2wsTxBbE', 'gpt-3.5-turbo-1106')")
-cursor_as.execute("SELECT * FROM assistants")
-print(cursor_as.fetchall())
-
-#create file
-#cursor_as.execute("""INSERT INTO files (file_id, assistant_id) VALUES ('file-zgD45ZrfmD8439jYTbAPddYO', 'asst_x824apYmRJibSkPy2wsTxBbE')""")
-cursor_as.execute("SELECT * FROM files")
-print(cursor_as.fetchall())
-
 # Commit the changes
 connection.commit()
 connection_as.commit()
